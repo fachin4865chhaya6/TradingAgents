fa9d323e42c5a416b7ff4bff9740285834471dda
Fachin Chhaya <fachin4865chhaya6@users.noreply.github.com>
2026-05-07 17:34:18 +0800

    add __url__ and __author_email__ metadata fields to package init

--- tradingagents/__init__.py
+++ tradingagents/__init__.py
@@ -9,11 +9,14 @@ enhancements for enterprise use cases.
 Personal fork notes:
 - Forked for learning and experimentation with multi-agent trading systems
 - See NOTES.md for personal observations and experiment results
+- Added __author_email__ and __url__ for easier package metadata access
 """
 
 __version__ = "0.1.0"
 __author__ = "TradingAgents Contributors"
+__author_email__ = ""
 __license__ = "Apache-2.0"
+__url__ = "https://github.com/TauricResearch/TradingAgents"
 
 from tradingagents.graph.trading_graph import TradingAgentsGraph
 from tradingagents.default_config import DEFAULT_CONFIG
@@ -22,4 +25,5 @@ __all__ = [
     "TradingAgentsGraph",
     "DEFAULT_CONFIG",
     "__version__",
+    "__url__",
 ]
